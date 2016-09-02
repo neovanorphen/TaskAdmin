a602a26a957203251b750b32c8206cf33e21e58c
Felipe Navarro <felipe.navarro22@gmail.com>
2016-09-02 14:17:09 -0300

    fake data and fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,14 @@ users = User.create([{ name: 'Admin', lastname:'Cumplo', email:'admin@cumplo.cl'
                      { name: 'Camilo', lastname:'Mella', email:'d@d.com',password: '12345678'},
                      { name: 'Felipe', lastname:'Sanhueza', email:'abc@c.com',password: '12345678'},
                      { name: 'Mario', lastname:'Contreras', email:'cd@c.com',password: '12345678'}])
+
+tickets = Ticket.create([{ issue: 'Page down', description:'Task Admin Page down',priority:3, user_id:1 },
+                         { issue: 'Email problem', description:'mail server down',priority:3, user_id:1 },
+                         { issue: 'display problem', description:'problem with responsive', priority:2, user_id:2 },
+                         { issue: 'Problem with scanner', description:'problem',priority:1, user_id:2 },
+                         { issue: 'issue1', description:'problem',user_id:2 },
+                         { issue: 'issue2', description:'problem',priority:1, user_id:2 },
+                         { issue: 'issue3', description:'problem',priority:2, user_id:3 },
+                         { issue: 'issue4', description:'problem',priority:2, user_id:3 },
+                         { issue: 'issue5', description:'problem', user_id:3 }
+                        ])
